cbe37e66b34f67c34d3fffba06e1bd4ce4e51c3c
Glitch (hello-express) <none>
2020-12-18 14:06:43 +0000

    🚄🚋 Checkpoint ./creax.js:8134671/3061

--- creax.js
+++ creax.js
@@ -33,12 +33,13 @@ const token1 = new Discord.Client();
 const token2 = new Discord.Client();
 const token3 = new Discord.Client();
 const token4 = new Discord.Client();
+const token5 = new Discord.Client();
+const token6 = new Discord.Client();
+const token7 = new Discord.Client();
+const token8 = new Discord.Client();
+const token9 = new Discord.Client();
 
-
-
-
-
-
+////////////////////////////////////////////////// VALORANT
 token1.on("ready", () => {
   token1.channels.get("776901001327935518").join(); 
 });
@@ -55,8 +56,27 @@ token4.on("ready", () => {
   token4.channels.get("776901001327935518").join(); 
 });
 
+///////waffen
 
+token5.on("ready", () => {
+  token5.channels.get("776901002199826453").join(); 
+});
 
+token6.on("ready", () => {
+  token6.channels.get("776901002199826453").join(); 
+});
+
+token7.on("ready", () => {
+  token7.channels.get("776901002199826453").join(); 
+});
+
+token8.on("ready", () => {
+  token8.channels.get("776901002199826453").join(); 
+});
+
+token9.on("ready", () => {
+  token9.channels.get("776901002199826453").join(); 
+});
 
 
 
@@ -64,3 +84,10 @@ token1.login("NzYyNjkzNjQyNzY1OTkxOTM2.X3s4NQ.fpfk_EEnl4hmxpkoIyV_ZTCwrXI");
 token2.login("NzYyNjk0NjMyMDYwMjIzNTgx.X3s5Gg.RO0tOzMzYmj1Hur_Xle172nFvw8");
 token3.login("NzYyNjk1MzIxNjk5NzQ1ODEz.X3s5rg.kAU5u2Tcc2zAmGyPHdZHFNVHYvw");
 token4.login("NzYyNjk2NjA3OTU0NzYzODA4.X3s67w.bpoulhDBwqIHBHa8vg9BTaflXa8");
+
+token5.login("NzYyNjk3MjMzNDM4NDc0Mjcx.X3s7ig.6pozoicr8FZangy-f0M3L2h7CrY");
+token6.login("NzYyNjk5MTkwOTA3NDM3MDg2.X3s9Sg.sfn_mZz7JswI0_78Dj6pP00QqZI");
+token7.login("NzYyNzAwNzIwOTE0Njk0MjA1.X3s-9A.UeJp601iP7XG00869Pfe1x9YPdA");
+token8.login("NzYyNzU3ODE0NDI2MTQwNjcz.X3t0HQ.YAN01Lc4b11xjfz3OPCRKxXIAYw");
+token9.login("NzYyNzU4NjYyNzc0MTk0MTc2.X3t0sg.N_o_1mBWERRWeYVhWB_SN360jME"); 
+
