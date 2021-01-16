1e5be6603b52863d14a0d319d7326005e24038d3
Glitch (hello-express) <none>
2021-01-16 21:59:40 +0000

    📟👾 Checkpoint ./creax.js:22891418/15932 ./package.json:22891418/22 ./hypnos.js:22891418/52

---
+++ hypnos.js
@@ -0,0 +1,234 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + "  Token Sese Sokma | Hostlandı");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const client1 = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+
+
+
+const data = new Map();
+
+
+
+client.login("tokenlerburaya")//Hesap Tokeni Token
+
+client.on('ready', ()=>{
+client.channels.get('kanalidlerburyaa').join()
+})
+
+client1.login("")
+
+client1.on('ready', ()=>{
+client1.channels.get('791679885835763744').join()
+})
+
+
+client2.login("")
+
+client2.on('ready', ()=>{
+client2.channels.get('791679885835763744').join()
+})
+
+
+client3.login("")
+
+client3.on('ready', ()=>{
+client3.channels.get('791679885835763744').join()
+})
+
+
+
+client4.login("")
+
+client4.on('ready', ()=>{
+client4.channels.get('791679885835763744').join()
+})
+
+
+client5.login("")
+
+client5.on('ready', ()=>{
+client5.channels.get('791679885835763744').join()
+})
+
+
+client6.login("")
+
+client6.on('ready', ()=>{
+client6.channels.get('791679885835763744').join()
+})
+
+
+client7.login("")
+
+client7.on('ready', ()=>{
+client7.channels.get('791679885835763744').join()
+})
+
+
+client8.login("")
+
+client8.on('ready', ()=>{
+client8.channels.get('791679885835763744').join()
+})
+
+
+client9.login("")
+
+client9.on('ready', ()=>{
+client9.channels.get('791679885835763744').join()
+})
+
+
+client10.login("")
+
+client10.on('ready', ()=>{
+client10.channels.get('791679885835763744').join()
+})
+
+
+client11.login("")
+
+client11.on('ready', ()=>{
+client11.channels.get('791679885835763744').join()
+})
+
+
+client12.login("")
+
+client12.on('ready', ()=>{
+client12.channels.get('791679885835763744').join()
+})
+
+
+client13.login("")
+
+client13.on('ready', ()=>{
+client13.channels.get('791679885835763744').join()
+})
+
+
+client14.login("")
+
+client14.on('ready', ()=>{
+client14.channels.get('791679885835763744').join()
+})
+
+
+client15.login("")
+
+client15.on('ready', ()=>{
+client15.channels.get('791679885835763744').join()
+})
+
+
+client16.login("")
+
+client16.on('ready', ()=>{
+client16.channels.get('791679885835763744').join()
+})
+
+
+client17.login("")
+
+client17.on('ready', ()=>{
+client17.channels.get('791679885835763744').join()
+})
+
+
+client18.login("")
+
+client18.on('ready', ()=>{
+client18.channels.get('791679885835763744').join()
+})
+
+
+client19.login("")
+
+client19.on('ready', ()=>{
+client19.channels.get('791679885835763744').join()
+})
+
+
+client20.login("")
+
+client20.on('ready', ()=>{
+client20.channels.get('791679885835763744').join()
+})
+
+
+client21.login("")
+
+client21.on('ready', ()=>{
+client21.channels.get('791679885835763744').join()
+})
+
+
+client22.login("")
+
+client22.on('ready', ()=>{
+client22.channels.get('791679885835763744').join()
+})
+
+
+client23.login("")
+
+client23.on('ready', ()=>{
+client23.channels.get('791679893788164096').join()
+})
+
+
+client24.login("")
+
+client24.on('ready', ()=>{
+client24.channels.get('791679893788164096').join()
+})
+
+
+client25.login("")
+
+client25.on('ready', ()=>{
+client25.channels.get('791679893788164096').join()
+})
