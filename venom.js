bc2b13beb3640de0b8d345b3f16b9b60cc0a9405
Glitch (hello-express) <none>
2021-03-04 15:25:57 +0000

    📙👵 Checkpoint ./hypnos.js:28036204/785 ./package.json:28036204/22

--- venom.js
+++ venom.js
@@ -52,30 +52,31 @@ const data = new Map();
 
 
 
-client.login("NzkxNzQ4OTQwMTkzODU3NTQ2.X-Tr5A.v0zhCOwVx3pLIiGXxUMwFemJk3k")//Hesap Tokeni Token
+client.login("TOKEN GİR")//Hesap Tokeni Token
+
 
 client.on('ready', ()=>{
-client.channels.get('799757561351503886').join()
+client.channels.get('KANAL ID').join()
 })
 
 client1.login("")
 
 client1.on('ready', ()=>{
-client1.channels.get('791679885835763744').join()
+client1.channels.get('KANAL ID').join()
 })
 
 
 client2.login("")
 
 client2.on('ready', ()=>{
-client2.channels.get('791679885835763744').join()
+client2.channels.get('KANAL ID').join()
 })
 
 
 client3.login("")
 
 client3.on('ready', ()=>{
-client3.channels.get('791679885835763744').join()
+client3.channels.get('KANAL ID').join()
 })
 
 
@@ -83,152 +84,152 @@ client3.channels.get('791679885835763744').join()
 client4.login("")
 
 client4.on('ready', ()=>{
-client4.channels.get('791679885835763744').join()
+client4.channels.get('KANAL ID').join()
 })
 
 
 client5.login("")
 
 client5.on('ready', ()=>{
-client5.channels.get('791679885835763744').join()
+client5.channels.get('KANAL ID').join()
 })
 
 
 client6.login("")
 
 client6.on('ready', ()=>{
-client6.channels.get('791679885835763744').join()
+client6.channels.get('KANAL ID').join()
 })
 
 
 client7.login("")
 
 client7.on('ready', ()=>{
-client7.channels.get('791679885835763744').join()
+client7.channels.get('KANAL ID').join()
 })
 
 
 client8.login("")
 
 client8.on('ready', ()=>{
-client8.channels.get('791679885835763744').join()
+client8.channels.get('KANAL ID').join()
 })
 
 
 client9.login("")
 
 client9.on('ready', ()=>{
-client9.channels.get('791679885835763744').join()
+client9.channels.get('KANAL ID').join()
 })
 
 
 client10.login("")
 
 client10.on('ready', ()=>{
-client10.channels.get('791679885835763744').join()
+client10.channels.get('KANAL ID').join()
 })
 
 
 client11.login("")
 
 client11.on('ready', ()=>{
-client11.channels.get('791679885835763744').join()
+client11.channels.get('KANAL ID').join()
 })
 
 
 client12.login("")
 
 client12.on('ready', ()=>{
-client12.channels.get('791679885835763744').join()
+client12.channels.get('KANAL ID').join()
 })
 
 
 client13.login("")
 
 client13.on('ready', ()=>{
-client13.channels.get('791679885835763744').join()
+client13.channels.get('KANAL ID').join()
 })
 
 
 client14.login("")
 
 client14.on('ready', ()=>{
-client14.channels.get('791679885835763744').join()
+client14.channels.get('KANAL ID').join()
 })
 
 
 client15.login("")
 
 client15.on('ready', ()=>{
-client15.channels.get('791679885835763744').join()
+client15.channels.get('KANAL ID').join()
 })
 
 
 client16.login("")
 
 client16.on('ready', ()=>{
-client16.channels.get('791679885835763744').join()
+client16.channels.get('KANAL ID').join()
 })
 
 
 client17.login("")
 
 client17.on('ready', ()=>{
-client17.channels.get('791679885835763744').join()
+client17.channels.get('KANAL ID').join()
 })
 
 
 client18.login("")
 
 client18.on('ready', ()=>{
-client18.channels.get('791679885835763744').join()
+client18.channels.get('KANAL ID').join()
 })
 
 
 client19.login("")
 
 client19.on('ready', ()=>{
-client19.channels.get('791679885835763744').join()
+client19.channels.get('KANAL ID').join()
 })
 
 
 client20.login("")
 
 client20.on('ready', ()=>{
-client20.channels.get('791679885835763744').join()
+client20.channels.get('KANAL ID').join()
 })
 
 
 client21.login("")
 
 client21.on('ready', ()=>{
-client21.channels.get('791679885835763744').join()
+client21.channels.get('KANAL ID').join()
 })
 
 
 client22.login("")
 
 client22.on('ready', ()=>{
-client22.channels.get('791679885835763744').join()
+client22.channels.get('KANAL ID').join()
 })
 
 
 client23.login("")
 
 client23.on('ready', ()=>{
-client23.channels.get('791679893788164096').join()
+client23.channels.get('KANAL ID').join()
 })
 
 
 client24.login("")
 
 client24.on('ready', ()=>{
-client24.channels.get('791679893788164096').join()
+client24.channels.get('KANAL ID').join()
 })
 
 
 client25.login("")
 
 client25.on('ready', ()=>{
-client25.channels.get('791679893788164096').join()
+client25.channels.get('KANAL ID').join()
 })
